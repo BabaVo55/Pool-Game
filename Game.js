ab0c1e932e5316bf1b1a082c8c3774e2c6e4f509
BabaVo55 <ethemcode@outlook.com>
2025-05-13 23:42:50 +0100

    complex objects

--- Game.js
+++ Game.js
@@ -4,12 +4,31 @@ function Game(){
 
 Game.prototype.init = function () {
 
+    this.gameWorld = new GameWorld();
 }
 
 Game.prototype.start = function () {
 
+    // Doesnt work??
+    // this.init()
+
+    // this.mainLoop()
+
+    // This Does though?
+    PoolGame.init()
+
+    PoolGame.mainLoop()
+
 }
 
 Game.prototype.mainLoop = function() {
-    
+
+    Canvas.clear();
+    PoolGame.gameWorld.updated();
+    PoolGame.gameWorld.draw();
+
+    requestAnimationFrame(PoolGame.mainLoop)
+
 }
+
+let PoolGame = new Game()
--- GameWordl.js
+++ GameWordl.js
@@ -7,5 +7,5 @@ GameWorld.prototype.updated = function() {
 }
 
 GameWorld.prototype.draw = function() {
-    
+    Canvas.drawImage(sprites.background, {x:0,y:0});
 }
