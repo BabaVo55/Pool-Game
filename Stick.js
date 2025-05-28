71d77c4a07582b93351c41aeb02b6e2d7121d273
BabaVo55 <ethemcode@outlook.com>
2025-05-28 07:45:17 +0100

    another change

--- Stick.js
+++ Stick.js
@@ -1,25 +1,3 @@
-// function Stick(){
-//     this.position = {x:0, y:400};
-// }
-
-// Stick.prototype.update = function(){
-
-//     //Testing
-//     if (this.position.x >= 0 && this.position.x <= 200){
-//         this.position.x ++
-//     } else {
-//         this.position.x --
-//     }
-
-// }
-
-// The above has a paradoxical buggy logic as we say if position.x > 0 
-// and < 200 to go forward with position.x ++ | however when the later logic kicks in
-// to push the stick back when it equals or exceeds to 200 then both logics
-// clash and the stick is frozen since it goes back and forth between the two
-// pixels - 199 - 200
-
-
 const STICK_ORIGIN = new Vector2(970, 11)
 // Below the logic is governed 
 function Stick(position) {
@@ -56,6 +34,30 @@ Stick.prototype.increasePower = function(){
     this.power += 100;
     this.origin.x += 5;
 }
+
+
+
+// function Stick(){
+//     this.position = {x:0, y:400};
+// }
+
+// Stick.prototype.update = function(){
+
+//     //Testing
+//     if (this.position.x >= 0 && this.position.x <= 200){
+//         this.position.x ++
+//     } else {
+//         this.position.x --
+//     }
+
+// }
+
+// The above has a paradoxical buggy logic as we say if position.x > 0 
+// and < 200 to go forward with position.x ++ | however when the later logic kicks in
+// to push the stick back when it equals or exceeds to 200 then both logics
+// clash and the stick is frozen since it goes back and forth between the two
+// pixels - 199 - 200
+
 
 // class Stick {
 //     constructor(){
--- Venctor2.js
+++ Venctor2.js
@@ -1,4 +1,4 @@
-function Vector2(x = 0,y = 0){
+function Vector2(x = 0, y = 0){
 
     // this.x = typeof x !== 'undefined' ? x : 0;
     // this.y = typeof y !== 'undefined' ? y : 0;
